acd0af6b7fabe905f54e3bd96417ace83606cc54
Lu Xia <vivian.xia.lu@gmail.com>
2020-03-15 22:48:24 +0800

    update xss demo

--- xssdemo/views.py
+++ xssdemo/views.py
@@ -3,20 +3,14 @@ from django.shortcuts import render
 msg = []
 def comment(request):
     if request.method == "GET":
-        return render(request,'comment.html')
+        return render(request, 'comment.html')
     else:
         v = request.POST.get('content')
         if "script" in v:
-            return render(request,'comment.html',{'error': 'error'})
+            return render(request, 'comment.html', {'error': 'error'})
         else:
             msg.append(v)
-            return render(request,'comment.html')
+            return render(request, 'index.html', {'msg': msg})
 
 def index(request):
-    return render(request,'index.html',{'msg':msg})
-
-def test(request):
-    from django.utils.safestring import mark_safe
-    temp = "<a href='http://www.baidu.com'>baidu</a>"
-    newtemp = mark_safe(temp)
-    return render(request,'test.html',{'temp':newtemp})
+    return render(request, 'index.html', {'msg': msg})
